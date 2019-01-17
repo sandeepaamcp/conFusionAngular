97ba08081abd608baa987ac22ff693fc8a53d1a5
sandeepa amcp <sandeepaamcp@gmail.com>
2019-01-17 22:29:44 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {
+  MatToolbarModule,
+  MatListModule,
+  MatButtonModule,
+  MatCardModule,
+  MatLineModule, MatGridListModule
+} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatListModule,
+    MatLineModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,12 @@
+//the shared folder is the common folder 
+//for all the components
+
+export class Dish{
+    name: string;
+    image: string;
+    category: string;
+    label: string;
+    price: string;
+    description: string;
+}
+
